d3936fc7f526a7bc6cee901cfd232dfa5bceaa28
316252667 <rodrigovelazquez@ciencias.unam.mx>
2019-01-10 16:24:31 -0600

    En Python 2 hicimos en grupo un programa para poder resolver un problema de fisica

---
+++ Clases/Programas/Ejemplo1 Pelota.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#Velázquez Larriva Rodrigo Gibran
+print 34*3 - 1/2 * 9.81*3**2 #1/2 Calcula la división
+print 34*3 - 1.0/2 * 9.81*3**2 #1.0/2 division flotante
+print 34*1 - 1.0/2 * 9.81*1**2
+print 34*1.5 - 1.0/2 * 9.81*1.5**2
+print 34*5 - 1.0/2 * 9.81*5**2
+v0 = 34
+g = 9.81
+t = 5
+y = v0*t - 1.0/2*g*t**2
+print y
+
+ 
+ 
