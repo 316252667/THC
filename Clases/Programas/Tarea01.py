f1877068e369a9c1ec3f443d426a7210c4931cea
316252667 <rodrigovelazquez@ciencias.unam.mx>
2019-01-11 13:03:38 -0600

    Esta es la primer tarea, en la que tuvimos que hacer que python nos resolviera un problema de fisica, en este caso MRU

---
+++ Clases/Programas/Tarea01.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#Velázquez Larriva Rodrigo Gibran
+#Rodrigo tenia que entregar unos documentos a  2km de su casa,
+#suponiendo que toma la bicicleta y hace el recorrido sin parar,
+#a una velocidad de 10 km/hr. ¿En cuanto tiempo regresa a su casa?
+v = 10 
+d = 2.0
+t = d/v #Como nos pide el tiempo, despejamos la formula anterior
+print t #este resultado es solo de ida
+print str (t)+ " hrs"
+print 2*t #multiplicando por 2 es ida y vuelta
+#Tardó 0.4 hrs ó 24 minutos en regresar
